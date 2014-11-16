2f6122eed7fa9ca459c9052fcfbb40fb5827f006
Robert Wild <wildr2@rpi.edu>
2014-11-16 02:10:15 -0500

    Initial Commit

---
+++ AvalancheVR/Assets/BlockSpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockSpawner : MonoBehaviour 
+{
+    public Transform block_prefab;
+    public float spawn_square_width = 10;
+    public Rigidbody rigid;
+
+    public float width_min = 2, width_max = 3;
+
+    private float spawn_time_max = 2f, spawn_time_min = 0.5f;
+    private float spawn_timer;
+
+
+    public void Start()
+    {
+        SetTimer();
+    }
+
+    public void Update()
+    {
+        spawn_timer -= Time.deltaTime;
+
+        if (spawn_timer <= 0)
+        {
+            SpawnBlock();
+            SetTimer();
+        }
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        rigidbody.isKinematic = true;
+    }
+
+    private void SpawnBlock()
+    {
+        float w = spawn_square_width / 2f;
+        float x = Random.Range(transform.position.x - w, transform.position.x + w);
+        float z = Random.Range(transform.position.z - w, transform.position.z + w);
+        Vector3 pos = new Vector3(x, transform.position.y, z);
+        Transform block = (Transform)Instantiate(block_prefab, pos, Quaternion.identity);
+
+        float scale = Random.Range(width_min, width_max);
+        block.transform.localScale = new Vector3(scale, scale, scale);
+    }
+    private void SetTimer()
+    {
+        spawn_timer = Random.Range(spawn_time_min, spawn_time_max);
+    }
+}
