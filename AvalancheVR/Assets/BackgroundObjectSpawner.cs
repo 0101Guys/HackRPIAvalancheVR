7dc168091d1226bb60448a85cce2d9469c5d5803
Ginoel Teng <yarkovish@gmail.com>
2014-11-16 04:01:02 -0500

    Updated bg test scene and sphere spawner script, changed sphere textures

--- AvalancheVR/Assets/BackgroundObjectSpawner.cs
+++ AvalancheVR/Assets/BackgroundObjectSpawner.cs
@@ -9,6 +9,7 @@ public class BackgroundObjectSpawner : MonoBehaviour
 	public float spawn_circle_width = 75;
 	
 	public float width_min = 3, width_max = 9;
+	public float inner_dist = 12, outer_dist = 75;
 	
 	private float spawn_time_max = 2f, spawn_time_min = 0.5f;
 	private float spawn_timer;
@@ -35,37 +36,42 @@ public class BackgroundObjectSpawner : MonoBehaviour
 		float w = bs.spawn_square_width / 2f;
 		bool b;
 		float x, z;
-		if (Random.Range (0, 1) == 0)
-						b = true;
-				else
-						b = false;
-		if (b)
-			x = Random.Range(bs.transform.position.x + w + 12, bs.transform.position.x + w + 75);
-		else
-			x = Random.Range(bs.transform.position.x - w - 75, bs.transform.position.x - w - 12);
-		if (Random.Range (0, 1) == 0)
-			b = true;
-		else
-			b = false;
-		if (b)
-			z = Random.Range(bs.transform.position.z + w + 12, bs.transform.position.z + w + 75);
-		else
-			z = Random.Range(bs.transform.position.z + w - 75, bs.transform.position.z + w + 12);
-		Vector3 pos = new Vector3(x, Random.Range (transform.position.y - 12, transform.position.y + 46), z);
 		Transform sphere;
+		Vector3 pos;
 
-		if (Random.Range (0, 2) == 0)
-			b = true;
-		else
-			b = false;
-		Debug.Log(b);
-		if(b)
-			sphere = (Transform)Instantiate(sphere_prefab, pos, Quaternion.identity);
-		else
-			sphere = (Transform)Instantiate(sphere_prefabx, pos, Quaternion.identity);
+		for (int i = 0; i < 5; i++) {
+						if (Random.Range (0, 2) == 0)
+								b = true;
+						else
+								b = false;
+						if (b)
+								x = Random.Range (bs.transform.position.x + w + inner_dist, bs.transform.position.x + w + outer_dist);
+						else
+				x = Random.Range (bs.transform.position.x - w - outer_dist, bs.transform.position.x - w - inner_dist);
+						if (Random.Range (0, 2) == 0)
+								b = true;
+						else
+								b = false;
+						if (b)
+								z = Random.Range (bs.transform.position.z + w + inner_dist, bs.transform.position.z + w + outer_dist);
+						else
+								z = Random.Range (bs.transform.position.z + w - outer_dist, bs.transform.position.z + w - inner_dist);
+						pos = new Vector3 (x, Random.Range (transform.position.y - 12, transform.position.y + 46), z);
+						
+
+						if (Random.Range (0, 2) == 0)
+								b = true;
+						else
+								b = false;
+						Debug.Log (b);
+						if (b)
+								sphere = (Transform)Instantiate (sphere_prefab, pos, Quaternion.identity);
+						else
+								sphere = (Transform)Instantiate (sphere_prefabx, pos, Quaternion.identity);
 		
-		float scale = Random.Range(width_min, width_max);
-		sphere.transform.localScale = new Vector3(scale, scale, scale);
+						float scale = Random.Range (width_min, width_max);
+						sphere.transform.localScale = new Vector3 (scale, scale, scale);
+				}
 	}
 	private void SetTimer()
 	{
