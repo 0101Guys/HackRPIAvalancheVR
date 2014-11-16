b742c4dca1bece4a2d2fc1255e619c5c14290aea
jump4r <morria3@rpi.edu>
2014-11-16 05:10:13 -0600

    Added Wind functionality working on a copy of the player script.

---
+++ AvalancheVR/Assets/Scripts/Wind.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+	public GameObject player;
+	PlayerMove2 playerMovement;
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		playerMovement = player.GetComponent<PlayerMove2>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col) {
+		player.GetComponent<PlayerMove2>().SetWindBoostState (true);
+		Debug.Log ("Activate Wind boost");
+	}
+
+	void OnTriggerExit(Collider col) {
+		player.GetComponent<PlayerMove2>().SetWindBoostState(false);
+	}
+}
