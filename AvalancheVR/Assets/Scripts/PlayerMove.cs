89f1d041b2ceb9a4a29685db7f3b0af617b165f6
Robert Wild <wildr2@rpi.edu>
2014-11-16 07:00:39 -0500

    Movement fixes

--- AvalancheVR/Assets/Scripts/PlayerMove.cs
+++ AvalancheVR/Assets/Scripts/PlayerMove.cs
@@ -3,20 +3,38 @@ using System.Collections;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float move_speed = 5;
-    public float upwards_speed = 5f;
+    public float move_speed = 5f;
+    public float upwards_speed = 15f;
+
+    public Camera left_cam;
+    public Transform cam_rig;
+
 
     public void Update()
+    {
+
+        // mouse rotation
+        float euler_y = cam_rig.rotation.eulerAngles.y + Input.GetAxis("Mouse X") * move_speed;
+        cam_rig.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, euler_y, transform.rotation.eulerAngles.z));
+    }
+
+    public void FixedUpdate()
     {
         float input_x = Input.GetAxis("Horizontal");
         float input_y = Input.GetAxis("Vertical");
         bool input_jump = Input.GetButton("Jump");
 
-        transform.Translate(new Vector3(input_x, 0, input_y) * Time.deltaTime * move_speed);
 
+        // move
+        Vector3 fwrd = left_cam.transform.rotation * Vector3.forward;
+        transform.Translate(fwrd * input_y * Time.deltaTime);
+
+
+
+        // upwards 
         if (input_jump)
         {
-            rigidbody.AddForce(Vector3.up * upwards_speed);
+            rigidbody.AddForce(Vector3.up * upwards_speed * 50f * Time.deltaTime);
 
             // cap upwards velocity
             if (rigidbody.velocity.y > upwards_speed)
@@ -25,9 +43,6 @@ public class PlayerMove : MonoBehaviour
         }
 
 
-
-        // mouse rotation
-        float euler_y = Input.GetAxis("Mouse X");
-        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, euler_y, transform.rotation.eulerAngles.z));
+        
     }
 }
